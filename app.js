b333e215870e0ea21f5950d487aa7e817d80a0e0
Brittney Alfieri <vbrit95@hotmail.com>
2023-07-31 23:08:31 -0400

    Project 15 initial commits

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 const express = require("express");
-
-const app = express();
-
 const cors = require("cors");
-
 const mongoose = require("mongoose");
-
 const { errors } = require("celebrate");
-
 require("dotenv").config();
 
+const { errorHandler } = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const routes = require("./routes");
+
+const app = express();
 
 const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://0.0.0.0:27017/wtwr_db");
 
-const { errorHandler } = require("./middlewares/error-handler");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
-
 app.use(cors());
-
 app.use(express.json());
-
 app.use(requestLogger);
 app.use(routes);
-
 app.use(errorLogger);
-
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
