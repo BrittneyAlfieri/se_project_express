9a57a063adc081f044cdb55ce99eef5b0a11d350
Brittney Alfieri <vbrit95@hotmail.com>
2023-08-04 10:05:22 -0400

    Project 15 1st review changes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
-const { celebrate } = require("celebrate");
+// const { celebrate } = require("celebrate");
 const clothingItem = require("./clothingItems");
 const user = require("./users");
 const { createUser, login } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 const NotFoundError = require("../errors/not-found-error");
 
-const {
-  validateUserBody,
-  validateUserAuthentication,
-} = require("../middlewares/validation");
+// const {
+//   validateUserBody,
+//   validateUserAuthentication,
+// } = require("../middlewares/validation");
 
 router.use("/items", clothingItem);
 router.use("/users", auth.handleAuthError, user);
@@ -20,8 +20,11 @@ router.get(`/crash-rest`, () => {
   }, 0);
 });
 
-router.post("/signup", celebrate({ body: validateUserBody }), createUser);
-router.post("/signin", celebrate({ body: validateUserAuthentication }), login);
+router.post("/signup", createUser);
+router.post("/signin", login);
+
+// celebrate({ body: validateUserAuthentication }),
+// celebrate({ body: validateUserBody }),
 
 router.use(() => {
   throw new NotFoundError("NotFoundError");
